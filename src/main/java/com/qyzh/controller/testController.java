ed18e8db4d6a7a3d7ebf61e51f14b605691f5a63
CRH <chenrh@i-earthworm.com>
2017-12-07 16:33:46 +0800

    IDEA测试提交+1

--- src/main/java/com/qyzh/controller/testController.java
+++ src/main/java/com/qyzh/controller/testController.java
@@ -4,5 +4,6 @@ public class testController {
     public static void main(String args[]){
         System.out.println("hello word");
         System.out.println("no happy to say : Fuck");
+        System.out.println("duang");
     }
 }
