430cb72cd05c0aaf0b3a2409d124e2f4db4ba7b0
CRH <chenrh@i-earthworm.com>
2017-12-07 14:37:32 +0800

    IDEA测试提交

---
+++ src/main/java/com/qyzh/controller/testController.java
@@ -0,0 +1,8 @@
+package com.qyzh.controller;
+
+public class testController {
+    public static void main(String args[]){
+        System.out.println("hello word");
+        System.out.println("no happy to say : Fuck");
+    }
+}
